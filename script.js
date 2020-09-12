0876fd56823c700b407a7c107ec981f1e46c7717
r3tryk <59620170+r3tryk@users.noreply.github.com>
2020-09-12 22:08:45 +0300

    Update script.js
    Сделали проверку коли-ва введенных символов меньше 50 , вопрос задается 2 раза . Так же проверяется на ввод пустой строки и нажатия пользователем кнопки "Отмена".

--- script.js
+++ script.js
@@ -9,16 +9,28 @@ let personalMovieDB = {
     privat : false
 };
 
-let a = prompt('Один из последних посмотренных фильмов?');
-console.log(oneQuestion);
-let b = prompt('На сколько вы оцените его?');
-console.log(secondQestion);
-let c = prompt('Один из последних посмотренных фильмов?');
-let d = prompt('На сколько вы оцените его?');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних посмотренных фильмов?', '');
+          b = prompt('На сколько вы оцените его?', '');
+          //нижняя строка проверяет : если пользователь нажал отмена(тоесть а = null)и также с б , пользователь не отправил пустую строку , и кол-во символов меньше 50.
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+        personalMovieDB.movies[a] = b;  
+    } else {
+        i--;
+    }
+    
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман!');
+} else {
+    console.log('Произошла ошибка');
+}
 
-
+console.log(personalMovieDB);
 
 
